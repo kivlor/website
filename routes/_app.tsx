6b5629cac31313e4475c17350c48eacb6df012cd
kivlor <hello@kivlor.com>
2022-09-16 08:41:17 +1000

    basic html in place without styles

---
+++ routes/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Andrew Gallagher, aka Kivlor.</title>        
+        
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+      </Head>
+
+      <Component />
+    </>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,26 @@
 /** @jsxImportSource preact */
 
+import { asset } from "$fresh/runtime.ts";
+
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      Hello world!
+    <div>
+      <figure className="avatar">
+        <img src={asset('/avatar.gif')} alt="Kivlor" />
+      </figure>
+
+      <section>
+        <p>Andrew Gallagher, <em>aka</em> Kivlor.</p>
+        <p>Maker, Coder, Tourist, Geek.</p>
+      </section>
+
+      <nav>
+        <ul>
+          <li><a href="http://github.com/kivlor" target="_blank">github</a></li>
+          <li><a href="http://instagram.com/kivlor" target="_blank">instagram</a></li>
+          <li><a href="http://twitter.com/kivlor" target="_blank">twitter</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
