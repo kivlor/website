02c2785aa401f034e1d570214fb3cf20d11fa9c4
kivlor <hello@kivlor.com>
2022-09-19 08:36:46 +1000

    add plausible

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,13 +4,17 @@ import { AppProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
+  const plausibleSite = Deno.env.get('PLAUSIBLE_SITE')
+
   return (
     <>
       <Head>
-        <title>Andrew Gallagher, aka kivlor</title>        
+        <title>Maker, Coder, Tourist, Geek.</title>
 
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+
+        {plausibleSite && (<script defer data-domain={plausibleSite} src="https://plausible.io/js/plausible.js"></script>)}
       </Head>
 
       <Component />
