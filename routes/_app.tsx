634a2475c2bed3ab850b1e98acff81bd205683df
kivlor <hello@kivlor.com>
2022-09-18 21:04:35 +1000

    styles and images

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,8 +7,8 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-        <title>Andrew Gallagher, aka Kivlor.</title>        
-        
+        <title>Andrew Gallagher, aka kivlor</title>        
+
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
       </Head>
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,23 +4,35 @@ import { asset } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
-    <div>
-      <figure className="avatar">
-        <img src={asset('/avatar.gif')} alt="Kivlor" />
-      </figure>
-
-      <section>
-        <p>Andrew Gallagher, <em>aka</em> Kivlor.</p>
-        <p>Maker, Coder, Tourist, Geek.</p>
+    <div class="m-auto py-8 w-screen h-screen bg-white border-t-4 border-blue-200">
+      <section class="py-8">
+        <figure class="m-auto rounded-full w-48 h-48 bg-blue-200">
+          <img src={asset('/avatar.png')} alt="Kivlor" />
+        </figure>
       </section>
 
-      <nav>
-        <ul>
-          <li><a href="http://github.com/kivlor" target="_blank">github</a></li>
-          <li><a href="http://instagram.com/kivlor" target="_blank">instagram</a></li>
-          <li><a href="http://twitter.com/kivlor" target="_blank">twitter</a></li>
-        </ul>
-      </nav>
+      <section class="py-8">
+        <p class="mb-2 font-semibold text-center text-3xl">
+          Andrew Gallagher, <em class="font-normal">aka</em> Kivlor
+        </p>
+        <p class="mb-2 font-light text-center text-2xl text-gray-400">
+          Maker, Coder, Tourist, Geek.
+        </p>
+      </section>
+
+      <section class="py-4">
+        <nav class="flex justify-center space-x-4">
+          <a href="http://github.com/kivlor" target="_blank" alt="github">
+            <img class="w-10 h-10" src={asset('/command-line-icon.svg')} />
+          </a>
+          <a href="http://instagram.com/kivlor" target="_blank" alt="instagram">
+            <img class="w-10 h-10" src={asset('/camera-icon.svg')} />
+          </a>
+          <a href="http://twitter.com/kivlor" target="_blank" alt="twitter">
+            <img class="w-10 h-10" src={asset('/chat-bubble-icon.svg')} />
+          </a>
+        </nav>
+      </section>
     </div>
   );
 }
