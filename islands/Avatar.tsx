ea31400ea426cb16539a8f65bae05b52a366c2e7
kivlor <hello@kivlor.com>
2022-09-26 08:18:22 +1000

    add some interactivity to the social icons

---
+++ islands/Avatar.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource preact */
+
+import { asset } from "$fresh/runtime.ts";
+
+import { hovering } from '../utils/state.ts';
+
+export default function Avatar() {
+  let background = 'bg-green-100';
+  switch (hovering.value) {
+    case 'github':
+      background = 'bg-gray-200';
+      break;
+    case 'instagram':
+      background = 'bg-pink-200';
+      break;
+    case 'linkedin':
+      background = 'bg-blue-200';
+      break;
+  }
+
+  return (
+    <figure class={`m-auto rounded-full w-40 md:w-48 h-40 md:h-48 ${background} transition-colors duration-300`}>
+      <img src={asset('/avatar.png')} alt="Kivlor" />
+    </figure>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,13 @@
 /** @jsxImportSource preact */
 
-import { asset } from "$fresh/runtime.ts";
-
-import { Camera, ChatBubble, CommandLine } from '../components/icons.tsx';
+import Avatar from '../islands/Avatar.tsx';
+import Links from '../islands/Links.tsx';
 
 export default function Home() {
   return (
-    <div class="m-auto py-4 md:py-8 w-screen h-screen bg-white border-t-4 border-blue-200">
+    <div class="m-auto py-4 md:py-8 w-screen h-screen bg-white">
       <section class="py-8">
-        <figure class="m-auto rounded-full w-40 md:w-48 h-40 md:h-48 bg-blue-200">
-          <img src={asset('/avatar.png')} alt="Kivlor" />
-        </figure>
+        <Avatar />
       </section>
 
       <section class="py-4 md:py-8">
@@ -18,21 +15,11 @@ export default function Home() {
           Andrew Gallagher, <em class="font-normal">aka</em> Kivlor
         </p>
 
-        <p class="mb-2 font-light text-center text-xl md:text-2xl text-gray-400">
+        <p class="mb-2 font-light text-center text-xl md:text-2xl text-gray-600">
           Maker, Coder, Tourist, Geek.
         </p>
 
-        <nav class="flex justify-center space-x-6 py-4">
-          <a href="http://github.com/kivlor" target="_blank" alt="github">
-            <CommandLine class="w-10 h-10 stroke-current stroke-1 text-gray-600" />
-          </a>
-          <a href="http://instagram.com/kivlor" target="_blank" alt="instagram">
-            <Camera class="w-10 h-10 stroke-current stroke-1 text-gray-600" />
-          </a>
-          <a href="http://twitter.com/kivlor" target="_blank" alt="twitter">
-            <ChatBubble class="w-10 h-10 stroke-current stroke-1 text-gray-600" />
-          </a>
-        </nav>
+        <Links />
       </section>
     </div>
   );
