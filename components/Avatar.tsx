a545b6a7a449553eb1c39fa5fcdb90bec3c34c82
kivlor <hello@kivlor.com>
2022-10-14 06:43:08 +1000

    repeating bg

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,7 @@ import { asset } from '$fresh/runtime.ts';
 
 export default function Avatar() {
   return (
-    <figure class={`m-auto w-32 md:w-40 h-32 md:h-40 bg-gray-200 dark:bg-gray-800 rounded-full`}>
+    <figure class={`m-auto w-32 md:w-40 h-32 md:h-40 bg-gray-300 dark:bg-gray-700 rounded-full`}>
       <img src={asset('/avatar.png')} alt="Kivlor" />
     </figure>
   );
