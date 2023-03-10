ddf3e7844bbd47c6d793a3f586ac336a09fba4ee
kivlor <hello@kivlor.com>
2023-03-11 09:40:52 +1000

    remove em

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -4,7 +4,7 @@ export default function Bio() {
   return (
     <div class="py-4">
       <p class="mb-2 font-semibold text-center text-gray-900 dark:text-gray-100 text-2xl">
-        Andrew Gallagher, <em class="font-thin text-xl">aka</em> Kivlor
+        Andrew Gallagher, <span class="font-thin text-xl">aka</span> Kivlor
       </p>
 
       <p class="mb-2 font-thin text-center text-gray-600 dark:text-gray-400 text-xl">
