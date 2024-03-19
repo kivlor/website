2455896ce2494361c8b8db49efc4a2fdf04bc665
kivlor <hello@kivlor.com>
2024-03-19 16:49:15 +1000

    remove resume component

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,11 +2,10 @@ import Avatar from "../components/Avatar.tsx";
 import Bio from "../components/Bio.tsx";
 import Lightbar from "../components/Lightbar.tsx";
 import Links from "../components/Links.tsx";
-import Resume from "../components/Resume.tsx";
 
 export default function Index() {
   return (
-    <div class="h-full">
+    <div class="h-dvh">
       <Lightbar />
 
       <div class="m-auto md:max-w-3xl">
